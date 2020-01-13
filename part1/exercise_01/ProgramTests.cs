3201d435484273669c6732c31b441333d9a428db
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-13 15:38:19 +0200

    Rework exercise 01 and folders

---
+++ part1/exercise_01/ProgramTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using Exercise_01;
+using NUnit.Framework;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestExercise01()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                Assert.AreEqual(sw.ToString(), "Hello World!\n", "Maybe you should try Console.WriteLine...");
+            }
+        }
+    }
+}
