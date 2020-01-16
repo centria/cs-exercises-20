f8851e5c8a3398db4908d0ea72e7c5fd22753afe
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 22:03:06 +0200

    Ex 11

---
+++ part1/variables/exercise_11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise_11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int dayCount = 100;
+            double hoursToLunch = 1;
+            string statement = "Are you sure?";
+
+            // DO NOT TOUCH THESE!
+            Console.WriteLine("Days to summer:");
+            Console.WriteLine(dayCount);
+            Console.WriteLine("Hours to lunch:");
+            Console.WriteLine(hoursToLunch);
+            Console.WriteLine("Coding is fun:");
+            Console.WriteLine(statement);
+
+        }
+    }
+}
---
+++ part1/variables/exercise_11/ProgramTests.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using exercise_11;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestExercise05()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                Assert.AreEqual("Days to summer:\n200\nHours to lunch:\n3.5\nCoding is fun:\nIt sure is!\n", sw.ToString().Replace("\r\n", "\n"), "Change the variable values!");
+            }
+        }
+
+                [Test]
+        public void TestCountWriteLines()
+        {
+            string code = File.ReadAllText("../../../Program.cs");
+            int count = Regex.Matches(code, "Console.WriteLine").Count;
+
+            StringAssert.Contains("Console.WriteLine(\"Days to summer:\");", code, "You were not supposed to change the WriteLines!");
+            StringAssert.Contains("Console.WriteLine(dayCount);", code, "You were not supposed to change the WriteLines!");
+            StringAssert.Contains("Console.WriteLine(\"Hours to lunch:\");", code, "You were not supposed to change the WriteLines!");
+            StringAssert.Contains("Console.WriteLine(hoursToLunch);", code, "You were not supposed to change the WriteLines!");
+            StringAssert.Contains("Console.WriteLine(\"Coding is fun:\");", code, "You were not supposed to change the WriteLines!");
+            StringAssert.Contains("Console.WriteLine(statement);", code, "You were not supposed to change the WriteLines!");
+
+            Assert.AreEqual(6, count, "You were not supposed to change the WriteLines!");
+        }
+    }
+}
