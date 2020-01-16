9b0d9dbc3ce56c7411397e29324b188368958aeb
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 21:06:58 +0200

    Ex 09

---
+++ part1/printing/exercise_09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise_09
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write your code here:
+            
+        }
+    }
+}
---
+++ part1/printing/exercise_09/ProgramTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using exercise_09;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestExercise05()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                var input = new StringReader("Ada");
+                Console.SetIn(input);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                Assert.AreEqual("Hello, how are you?\nThat's interesting, tell me more\nThank you for sharing!\n", sw.ToString().Replace("\r\n", "\n"), "Remember to tell hello!");
+            }
+        }
+                [Test]
+        public void TestCountReadLines()
+        {
+            string code = File.ReadAllText("../../../Program.cs");
+            int count = Regex.Matches(code, "Console.ReadLine").Count;
+
+            Assert.AreEqual(2, count, "You were supposed to use Console.ReadLine 2 times.");
+        }
+    }
+}
