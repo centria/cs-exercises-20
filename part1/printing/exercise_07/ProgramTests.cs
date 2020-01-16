7d503a2a20a50b44fb8787f8e3b7b63f95e6125b
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 20:52:41 +0200

    Ex 07

---
+++ part1/printing/exercise_07/ProgramTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using exercise_06;
+using NUnit.Framework;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestExercise05()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                var input = new StringReader("Are you printing the string?");
+                Console.SetIn(input);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                Assert.AreEqual("Give input!\nAre you printing the string?\nAre you printing the string?\nAre you printing the string?\n", sw.ToString().Replace("\r\n", "\n"), "Remember to ask for input and print it!");
+            }
+        }
+    }
+}
