291e2d61b2e2c8b96636ba95947e1fbeaf326035
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 20:51:21 +0200

    Ex 06

---
+++ part1/printing/exercise_06/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise_06
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give input!");
+            // Write your code here:
+            
+        }
+    }
+}
