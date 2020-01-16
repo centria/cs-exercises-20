f4fa627bf26806a05272371671369a57826d7185
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 20:36:04 +0200

    Exercise 4

---
+++ part1/printing/exercise_04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise_04
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = "Passport and floss!";
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ part1/printing/exercise_04/ProgramTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using exercise_01;
+using NUnit.Framework;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestExercise01()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                Assert.AreEqual("Passport and a toothbrush!\n", sw.ToString().Replace("\r\n", "\n"), "Check your code! The test requires exact match!");
+            }
+        }
+    }
+}
