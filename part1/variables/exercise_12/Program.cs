f6e4acc1680bb16c7977183a18cef881cb1f6f14
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 22:14:38 +0200

    Ex 12

--- part1/variables/exercise_12/Program.cs
+++ part1/variables/exercise_12/Program.cs
@@ -7,12 +7,6 @@ namespace exercise_12
     public static void Main(string[] args)
     {
       // Write your code here:
-      Console.WriteLine("Give a number!");
-      string userInput = Console.ReadLine();
-
-      // Convert string to integer type
-      int intValue = Convert.ToInt32(userInput);
-      Console.WriteLine("You gave " + intValue);
 
     }
   }
