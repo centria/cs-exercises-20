c2c4ecb90acc351332a22bc8fd26d80f5a9f14a7
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 20:28:34 +0200

    Exercise 03

---
+++ part1/printing/exercise_03/ProgramTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using exercise_03;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace ProgramTests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void TestPartsOfTextExist()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                StringAssert.Contains("Once upon a time", sw.ToString().Replace("\r\n", "\n"), "You're supposed to write the lyrics using 3 Console.Writelines.");
+                StringAssert.Contains("I was falling in love", sw.ToString().Replace("\r\n", "\n"), "You're supposed to write the lyrics using 3 Console.Writelines.");
+                StringAssert.Contains("Now I'm only falling apart", sw.ToString().Replace("\r\n", "\n"), "You're supposed to write the lyrics using 3 Console.Writelines.");
+            }
+        }
+
+        [Test]
+        public void TestStringMatch()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                // Save a reference to the standard output.
+                TextWriter stdout = Console.Out;
+
+                // Redirect standard output to variable.
+                Console.SetOut(sw);
+
+                // Call student's code
+                Program.Main(null);
+
+                // Restore the original standard output.
+                Console.SetOut(stdout);
+
+                // Assert
+                StringAssert.AreEqualIgnoringCase("Once upon a time\nI was falling in love\nNow I'm only falling apart\n", sw.ToString().Replace("\r\n", "\n"));
+            }
+        }
+
+        [Test]
+        public void TestCountWriteLines()
+        {
+            string code = File.ReadAllText("../../../Program.cs");
+            int count = Regex.Matches(code, "Console.WriteLine").Count;
+
+            Assert.AreEqual(1, count, "You were supposed to use Console.WriteLine only once!.");
+        }
+
+    }
+}
