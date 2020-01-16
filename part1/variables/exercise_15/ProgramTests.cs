655b2236c24716d6b984a1b849b92349c52fb73e
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 22:41:16 +0200

    Ex 15

---
+++ part1/variables/exercise_15/ProgramTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using exercise_15;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace ProgramTests
+{
+  [TestFixture]
+  public class TestProgram
+  {
+    [Test]
+    public void TestExercise15()
+    {
+      using (StringWriter sw = new StringWriter())
+      {
+        // Save a reference to the standard output.
+        TextWriter stdout = Console.Out;
+
+        // Redirect standard output to variable.
+        Console.SetOut(sw);
+
+        Console.SetOut(sw);
+
+        var data = String.Join(Environment.NewLine, new[]
+        {
+                "word",
+                "12",
+                "3.2",
+                "true"
+                });
+
+        Console.SetIn(new System.IO.StringReader(data));
+
+        // Call student's code
+        Program.Main(null);
+
+        // Restore the original standard output.
+        Console.SetOut(stdout);
+
+        // Assert
+        Assert.AreEqual("Give a string:\nGive an integer:\nGive a double:\nGive a boolean:\nYour string: word\nYour integer: 12\nYour double: 3.2\nYour boolean: True\n", sw.ToString().Replace("\r\n", "\n"), "Remember to ask for a number and print it with \"You gave...\" ");
+      }
+    }
+  }
+}
