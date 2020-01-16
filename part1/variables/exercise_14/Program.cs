e5a77405505fbd8d096a329235ea5eeede03ada6
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-16 22:30:18 +0200

    Ex 14

---
+++ part1/variables/exercise_14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise_14
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Give me anything!");
+      // Write your code here:
+
+    }
+  }
+}
