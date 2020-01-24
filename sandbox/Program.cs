01ca7c284fa486d60c3ca0bed3115978092ca5f1
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-24 13:37:49 +0200

    Add sandbox

---
+++ sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
