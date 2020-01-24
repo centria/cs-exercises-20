a0a3bc102e0d62afd012ff128d855e2c9d1115eb
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-01-24 14:10:10 +0200

    Ex 13 test fix?

--- part1/variables/exercise_13/ProgramTests.cs
+++ part1/variables/exercise_13/ProgramTests.cs
@@ -36,7 +36,7 @@ namespace ProgramTests
         Console.SetOut(stdout);
 
         // Assert
-        Assert.AreEqual("Give a number!\nYou gave "+ (12.4).ToString().Replace(",",".") +"\n", sw.ToString().Replace("\r\n", "\n"), "Remember to ask for a number and print it with \"You gave...\" ");
+        Assert.AreEqual("Give a number!\nYou gave "+ (12.4).ToString().Replace(",",".") +"\n", sw.ToString().Replace("\r\n", "\n").Replace(",","."), "Remember to ask for a number and print it with \"You gave...\" ");
       }
     }
 
