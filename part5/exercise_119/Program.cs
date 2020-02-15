ccf55c079c47faceef6ae01962f870db34fe4d89
Heikki Ahonen <heikki.ahonen@centria.fi>
2020-02-15 19:36:08 +0200

    Clean Program.cs

--- part5/exercise_119/Program.cs
+++ part5/exercise_119/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using static System.Linq.Enumerable;
 
 
 namespace exercise_119
@@ -9,23 +8,7 @@ namespace exercise_119
   {
     public static void Main(string[] args)
     {
-      Timer timer = new Timer();
-      Console.WriteLine(timer);
-      foreach (int i in Range(0,100)) {
-        timer.Advance();
-      }
-      Console.WriteLine(timer);
 
-      foreach (int i in Range(0,5899)) {
-        timer.Advance();
-      }
-      Console.WriteLine(timer);
-
-      timer.Advance();
-      Console.WriteLine(timer);
-      
-      timer.Advance();
-      Console.WriteLine(timer);
     }
   }
 }
